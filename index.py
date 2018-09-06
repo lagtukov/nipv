12099376ce89b8f8b5ba5d17c3f2a52065c259ee
lagtukov <38328119+lagtukov@users.noreply.github.com>
2018-09-06 11:17:29 +0200

    replace hello world text with NIPV test

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "NIPV test"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"))
